ef4166ab400087c69db1dda68a85736db6250b49
Pete Hodgson <git@thepete.net>
2016-11-20 14:49:12 -0800

    add example of a `class`-based implementation

--- test/money.spec.js
+++ test/money.spec.js
@@ -1,8 +1,20 @@
-const Money = require('../lib/money');
+const ThisLessMoney = require('../lib/money');
+const ClassBasedMoney = require('../lib/this-based-money');
 
-describe('a money value object', function () {
+describe('a class-based money value object', function () {
+  function createMoney(amount,currency){ 
+    return new ClassBasedMoney(amount,currency); 
+  }
+  describeMoney(createMoney);
+});
+
+describe('a this-less money value object', function () {
+  describeMoney(ThisLessMoney);
+});
+
+function describeMoney(createMoney){
   describe('10 USD', function () {
-    const tenDollars = Money(10,'USD');
+    const tenDollars = createMoney(10,'USD');
 
     it('has a dollar value', function () {
       expect(tenDollars).to.exist;
@@ -11,19 +23,19 @@ describe('a money value object', function () {
     });
 
     it('equals another 10 USD value', function () {
-      const anotherTenDollars = Money(10,'USD');
+      const anotherTenDollars = createMoney(10,'USD');
       expect(tenDollars.equals(anotherTenDollars)).to.be.true;
     });
 
     it('does not equals an 11 USD value', function () {
-      const elevenDollars = Money(11,'USD');
+      const elevenDollars = createMoney(11,'USD');
       expect(tenDollars.equals(elevenDollars)).to.be.false;
     });
 
     it('does not equals a 10 GBP value', function () {
-      const tenPounds = Money(10,'GBP');
+      const tenPounds = createMoney(10,'GBP');
       expect(tenDollars.equals(tenPounds)).to.be.false;
     });
     
   });
-});
+}
